0225248bad3177d15bcbf2f907918cd9a0552b1d
Carlos Boyzo <carlosboyzo@MacBook-Pro-de-Carlos-2.local>
2022-08-28 00:29:04 -0500

    init

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.scss'
+
+function App() {
+
+  return (
+    <div className="App">
+          <img src="/vortex.png" className="logo" alt="Vite logo" />
+      
+    </div>
+  )
+}
+
+export default App
