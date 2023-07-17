95b281e9762e5b4b0fd05faa67333fa09e81ed4c
crbzxy <carlos.boor@gmail.com>
2023-07-16 20:00:51 -0600

    update

--- src/components/Carrousel.jsx
+++ src/components/Carrousel.jsx
@@ -8,6 +8,7 @@ import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 import 'swiper/css/effect-fade';
 import '../styles/App.scss';
+import carouselData from '../utils/carouselData.json';
 
 // import required modules
 import { Autoplay, EffectFade, Pagination, Navigation } from 'swiper';
@@ -18,7 +19,6 @@ export default function Carrousel() {
       <div className='container flex align-middle content-center parallax'>
         <Swiper
           spaceBetween={30}
-
           pagination={{
             type: 'fraction',
           }}
@@ -30,44 +30,23 @@ export default function Carrousel() {
           navigation={true}
           loop={true}
           modules={[Autoplay, EffectFade, Pagination, Navigation]}
-          className='mySwiper'>
-          <SwiperSlide>
-            <div  >
-              <img src='/assets/Bicicleta.png' alt='Vite logo' />
-              <p className='titulo'>BY-cycle® 2022 campaign</p>
-              <p>Producción, shooting y dirección creativa - Julio 2022</p>
-              <div className='overlay'>
-            <iframe width={560} height={315} src="https://www.youtube.com/embed/pSU13TI6XGs" title="YouTube video player" frameBorder={0} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
-
-
-                
-              </div>
-            </div>
-          </SwiperSlide>
-
-          <SwiperSlide>
-            <a href='https://www.youtube.com/watch?v=__k27tMyZZg'>
-              <img src='/assets/carrousel.png' alt='Vite logo' />
-              <p className='titulo'>BY-cycle® 2022 campaign 2</p>
-              <p>Producción, shooting y dirección creativa - Julio 2022</p>
-              <div className='overlay'>
-                <div className='text'>Ver el reel</div>
-              </div>
-            </a>
-          </SwiperSlide>
-
-          <SwiperSlide>
-            <a href='https://www.youtube.com/watch?v=__k27tMyZZg'>
-              <img src='/assets/Bicicleta.png' alt='Vite logo' />
-              <p className='titulo'>BY-cycle® 2022 campaign 3</p>
-              <p>Producción, shooting y dirección creativa - Julio 2022</p>
-              <div className='overlay'>
-                <div className='text '>Ver el reel</div>
-              </div>
-            </a>
-          </SwiperSlide>
+          className='mySwiper'
+        >
+          {carouselData.map((slide, index) => (
+            <SwiperSlide key={index}>
+              <a href={slide.videoUrl}>
+                <img src={slide.image} alt='Vite logo' />
+                <p className='titulo'>{slide.title}</p>
+                <p>{slide.description}</p>
+                <div className='overlay'>
+                  <div className='text'>Ver el reel</div>
+                </div>
+              </a>
+            </SwiperSlide>
+          ))}
         </Swiper>
       </div>
     </>
   );
 }
+
